8e4451da868e60702c97ea601a94d1698bb9fae8
Carlos Korir <carlkorir226@gmail.com>
2025-06-30 11:49:40 +0300

    adds vite api base url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5555'; 
+
 const instance = axios.create({
-  baseURL: 'http://localhost:5555',
+  baseURL: API_BASE_URL, // Use the dynamically set URL
   headers: { 'Content-Type': 'application/json' },
 });
 
